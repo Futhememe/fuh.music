3710b4e4764334fcdfc9b1450e7264230032712c
Gustavo <kyoro_souza@hotmail.com>
2021-01-16 00:55:47 -0200

    :sparkles: new routes

---
+++ packages/auth/src/screens/Login/Login.tsx
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from 'react';
+
+import { AuthContext } from '../../providers/AuthProvider';
+
+const Login = ({ redirectWhenSignIn, history }) => {
+  const { authenticated } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (authenticated) history.push(redirectWhenSignIn);
+  }, [authenticated]);
+
+  return (
+    <LoginContainer>
+      <Logo />
+
+      <Paper>
+        <LoginForm />
+      </Paper>
+    </LoginContainer>
+  );
+};
+
+export default Login;
